57d00df798df701bd4ef0eb3a2268bd9644042e7
hjkhalid <hk810773@gmail.com>
2023-02-05 17:58:01 +0530

    rendred the card using news api data

---
+++ src/component/Api.js
@@ -0,0 +1,35 @@
+// /
+// import axios from 'axios';
+// import React from 'react';
+
+// class api extends React.Component {
+ 
+//   componentDidMount() {
+
+//      axios
+//       .get("https://newsapi.org/v2/top-headlines?country=us&apiKey=7e1aec59a251435dbe386f7ff6b7656f")
+//       .then(response => {
+//         this.setState({ data: response.data });
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }
+ 
+
+//   render() {
+//     return (<>
+//       <div>
+//         {this.state.data.map((item) => (
+//           <div key={item.id}>
+//             <h2>{item.title}</h2>
+//             <p>{item.body}</p>
+//           </div>
+//         ))}
+//       </div>
+//       </>
+//     );
+//   }
+// }
+// export default api;
+
